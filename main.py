a2ff06c52bbee53b5be4a481038d59074c76cb9f
sunny <sunnyroy8084401051@gmail.com>
2022-11-04 10:15:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+In this section we will see about how to create a program to different between vowel and consonant.
+_____________________________________________________________________________________________________
+Question : user is giving a character you have to fine whether the character is vowel or consonant?
+_____________________________________________________________________________________________________
+Approach : first take input form user and then compare the character to vowel if character match then print vowel else
+           print consonant.
+"""
+
+A = str(input("Enter the character to find whether it is vowel or consonant :"))
+if A == 'a':
+    print("this character is vowel :")
+else:
+    print("this character is consonant :")
+
+if A == 'e':
+          print("this character is vowel :")
+else:
+    print("this character is consonant :")
+if A == 'i':
+      print("this character is vowel :")
+else:
+ print("this character is consonant :")
+if A == 'o':
+     print("this character is vowel :")
+else:
+     print("this character is consonant :")
+if A == 'u':
+    print("this character is vowel :")
+else:
+    print("this character is consonant :")
